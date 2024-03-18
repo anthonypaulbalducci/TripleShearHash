c357b455c51f96ece8c931bdd56920b3470b6b47
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2024-03-17 21:23:11 -0400

    Update TripleShearHash.py

--- TripleShearHash.py
+++ TripleShearHash.py
@@ -1,27 +1,35 @@
+from PIL import Image
 import numpy as np
 import math as m
 
-# Define theta in degrees
-angle = 30
-theta = m.radians(angle)
+def load_image(image_path):
+    img = Image.open(image_path)
+    return np.array(img)
 
-# Triple shear transformation
+def rotate(image, angle)
+    # Define theta in degrees
+    angle = 30
+    theta = m.radians(angle)
 
-# first = np.array([[1, -np.tan(theta/2)], [0, 1]])
-# second = np.array([[1, 0], [np.sin(theta), 1]])
-# third = np.array([[1, -np.tan(theta/2)], [0, 1]])
+    # Triple shear transformation
 
-side = np.array([[1, -np.tan(theta/2)], [0, 1]])
-middle = np.array([[1, 0], [np.sin(theta), 1]])
+    # first = np.array([[1, -np.tan(theta/2)], [0, 1]])
+    # second = np.array([[1, 0], [np.sin(theta), 1]])
+    # third = np.array([[1, -np.tan(theta/2)], [0, 1]])
 
-# Define vector test set - X, Y
-pixels = np.array([[10, 5], [20, 3], [4, 5]])
+    side = np.array([[1, -np.tan(theta/2)], [0, 1]])
+    middle = np.array([[1, 0], [np.sin(theta), 1]])
 
-# Perform the matrix multiplication
+    # Define vector test set - X, Y
+    pixels = np.array([[10, 5], [20, 3], [4, 5]])
 
-# result = first @ second @ third @ pixels.T
-result = side @ middle @ side @ pixels.T
+    # Perform the matrix multiplication
 
-# Print the result
-print(np.round(result.T))
+    # result = first @ second @ third @ pixels.T
+    result = side @ middle @ side @ pixels.T
+
+    # Print the result
+    print(np.round(result.T))
+
+image = load_image('')
 
